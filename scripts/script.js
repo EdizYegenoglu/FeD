cbbf943028c6ad7acb3d5ba5d36b4d7fa02b13b9
EdizYegenoglu <74187746+EdizYegenoglu@users.noreply.github.com>
2020-11-26 15:35:08 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+
+// hamburgermenu
+var navbar = document.querySelector(".navbar")
+var ham = document.querySelector(".hamburgermenu")
+
+ham.addEventListener("click", toggleHamburger)
+
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+    navbar.classList.toggle("showNav")
+    ham.classList.toggle("showClose")
+  }
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)

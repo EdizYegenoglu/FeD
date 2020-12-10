4c99f937a6aab3668803d8eb83b94acb9fd0552a
EdizYegenoglu <74187746+EdizYegenoglu@users.noreply.github.com>
2020-12-10 15:24:02 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,20 @@
 // hamburgermenu
 var navbar = document.querySelector("header nav")
 var ham = document.querySelector("header button")
+var hamClose = document.querySelector("header ul li:first-of-type")
+var x = document.getElementById("overlayDing")
+
 
 ham.addEventListener("click", toggleHamburger)
+hamClose.addEventListener("click", toggleHamburger)
+x.addEventListener("click", toggleHamburger)
 
 // toggles hamburger menu in and out when clicking on the hamburger
 function toggleHamburger(){
     navbar.classList.toggle("showNav")
     ham.classList.toggle("showClose")
+    hamClose.classList.toggle("showClose")
+    // klikNaast.classList.toggle("showClose")
   }
 
 var menuLinks = document.querySelectorAll(".menuLink")
@@ -19,3 +26,29 @@ menuLinks.forEach(
     menuLink.addEventListener("click", toggleHamburger) 
   }
 )
+
+// Overlay van zwarte vlak & niet meer kunnen scrollen
+
+ham.addEventListener("click", myFunction)
+hamClose.addEventListener("click", myFunction)
+x.addEventListener("click", myFunction)
+
+
+var x = document.getElementById("overlayDing")
+var body = document.getElementById("paginaEen")
+
+function myFunction() {
+  
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    body.style.overflow = "scroll";
+  } else {
+    x.style.display = "block";
+    body.style.overflow = "hidden";
+  }
+  // if (x.style.opacity === "1") {
+  //   x.style.opacity = "0";
+  // } else {
+  //   x.style.opacity= "1";
+  // }
+}
